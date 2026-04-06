d0c58016904197553db5243c03284b91cac94599
SV3105 <snehavaghela3105@gmail.com>
2026-04-06 11:15:12 +0530

    customer_group module completed

---
+++ app/Models/Customergroup.php
@@ -0,0 +1,22 @@
+<?php 
+require_once 'app/Models/Core/Row.php';
+class Model_Customergroup extends Model_Core_Row {
+    public function __construct(){
+        parent::__construct();
+        $this->tableName = "customer_group";
+        $this->primaryKey = "customer_group_id";
+    }
+    
+    public function insert(){
+        $this->data['created_at'] = date('Y-m-d H:i:s');
+
+        return parent::insert();
+    }
+
+    public function update(){
+        $this->data['updated_at'] = date('Y-m-d H:i:s');
+
+        return parent::update();
+    }
+}
+?>
